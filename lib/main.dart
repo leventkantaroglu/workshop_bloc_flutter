f9fcc8588de0b0c0f9975825b5c1a11d8fe462b0
Levent KANTAROĞLU <leventkantaroglu@dusyeri.com.tr>
2022-03-06 11:27:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:workshop_bloc_flutter/app.dart';
+
+import 'counter_observer.dart';
+
+void main() {
+  BlocOverrides.runZoned(
+    () {
+      runApp(const CounterApp());
+    },
+    blocObserver: CounterObserver(),
+  );
+}
